61a84037d08b3f71fbb94948fc0ca7c14837ca85
Gareth A Rowell <gareth_rowell@nps.gov>
2020-02-03 13:38:04 -0600

    Add files via upload

--- examples/DataScanner.java
+++ examples/DataScanner.java
@@ -1,6 +1,10 @@
 
+
 import java.io.*;
 import java.util.Scanner;
+
+
+
 
 public class DataScanner {
 	
@@ -8,29 +12,32 @@ public class DataScanner {
 	String infile = new String();
 	int row, col;
 
-	DataScanner(String sourcefile) throws FileNotFoundException {
+
+	DataScanner(String sourcefile)throws FileNotFoundException {
 		
 		infile = sourcefile;
-		
 		try{
 			scanner = new Scanner(new BufferedReader(new FileReader(infile)));
+			System.out.println("Trying scanner");
 		}
 		catch(FileNotFoundException ex){
 			System.out.println(ex);
 		}
-      }
+    }
     
 	public String[][] load(int maxrow, int maxcol){
 		
 		String mydata[][] = new String[maxrow][maxcol];
 		
 		for(row = 0; row < maxrow; row++){
+			
 			String line = scanner.nextLine();
 			String parts[] = line.split(",");
+			
 			for(col = 0; col < maxcol; col++){
 				mydata[row][col] = parts[col];
 			}
-		}	
+		}
 		
 		return(mydata);
 	}
