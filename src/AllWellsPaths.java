f28d9b14ae79d3d073410ad18d7080eeb73e0bb1
Gareth A Rowell <gareth_rowell@nps.gov>
2020-02-07 15:37:59 -0600

    Add files via upload

--- src/AllWellsPaths.java
+++ src/AllWellsPaths.java
@@ -34,71 +34,9 @@ public class AllWellsPaths extends Application {
 		int offsetX = 50;
 		int offsetY = 25;
    
-		//Creating Y axis
-		Line yAxis = new Line(offsetX, offsetY, offsetX, offsetY + 500);
-		yAxis.setStrokeWidth(3.0); 
-		yAxis.setStroke(Color.WHITE);
-	  
-		Line xAxis = new Line(offsetX, offsetY + 250, offsetX + 1825, offsetY + 250);
-		xAxis.setStrokeWidth(3.0); 
-		xAxis.setStroke(Color.WHITE);
-	  
-		Group group = new Group(); 
-		group.getChildren().add(xAxis);
-		group.getChildren().add(yAxis);
-	  
-		//make five hash marks on y axis (at -200, -100, 0, 100, 200)
-		int yhashno = 5;  
-		Line line = null;
-		Text text = null;
-		//Text avetext = null;
-		for(int i = 0; i < yhashno; ++i){
-			line = new Line(offsetX, (offsetY + 450 - (i*100)), offsetX + 20, (offsetY + 450 - (i*100)));
-			line.setStrokeWidth(2.0);
-			line.setStroke(Color.WHITE);
-			group.getChildren().add(line);    
-			
-			text = new Text();
-			if(i != 2){
-				text.setText(String.valueOf(offsetY - 225 + (i*100)));
-				text.setX(offsetX + 30);
-				text.setY(offsetY + 455 - (i*100));
-				text.setStrokeWidth(1);
-				text.setStroke(Color.WHITE);
-				group.getChildren().add(text);
-			}
-			
-		}
-	  
-		//make ten major hash marks on X axis (at 365, 730, 1095, 1460, 1825)
-		int xhashno = 6; 
-		for(int i = 0; i < xhashno; ++i){
-			line = new Line(offsetX + (365*i), offsetY + 250, offsetX + (i*365), offsetY + 230);
-			line.setStrokeWidth(2.0); 
-			line.setStroke(Color.WHITE);
-			group.getChildren().add(line);  
-			
-			if(i > 0){
-				text = new Text();
-				text.setText(String.valueOf(2010 + 2*i));
-				text.setX(offsetX + (365*i - 12));
-				text.setY(offsetY + 220);
-				text.setStrokeWidth(1);
-				text.setStroke(Color.WHITE);
-				group.getChildren().add(text);
-			}
-			
-		}
-	  
-		//make five minor hash marks on X axis (at 365, 730, 1095, 1460, 1825)
-		yhashno = 6; 
-		for(int i = 0; i < yhashno; ++i){
-			line = new Line(offsetX + ((365*i) - 182), offsetY + 250, offsetX + (i*365)-182, offsetY + 240);
-			line.setStrokeWidth(1.0); 
-			line.setStroke(Color.WHITE);
-			group.getChildren().add(line);  
-		}
-	 
+		Group group = new Group();
+		XYAxes myXYAxes = new XYAxes();			
+		myXYAxes.addFeatures(group, offsetX, offsetY);
 	 
 		//------------------------------------------------------------------------------
 		// get data
@@ -107,6 +45,7 @@ public class AllWellsPaths extends Application {
 		int maxcol = 10;
 		int row = 1;
 		int col = 1;
+		boolean echo = true;
 		
 		//String welldata[][] = new String[maxrow][maxcol];
 		String[][] welldata = new String[maxrow][maxcol];
@@ -116,15 +55,7 @@ public class AllWellsPaths extends Application {
 	   
 		try{
 			DataScanner datascanner = new DataScanner(infile);
-			welldata = datascanner.load(maxrow, maxcol);  
-
-			for(row = 0; row < maxrow; row++){
-				System.out.println(row + " " + welldata[row][0] + " " + welldata[row][1] +
-				    " " +  welldata[row][2] + " " +  welldata[row][3] + 	
-					" " +  welldata[row][4] + " " +  welldata[row][5] + 
-					" " +  welldata[row][6] + " " +  welldata[row][7] +
-					" " +  welldata[row][8] + " " +  welldata[row][9]);
-			}	  
+			welldata = datascanner.load(maxrow, maxcol, echo);  
 		}	  
 		catch (FileNotFoundException ex){
 			System.out.println(ex);
