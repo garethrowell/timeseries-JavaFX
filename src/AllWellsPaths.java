e6a0ca713d4d4a8b99325a6d7d190af9f302d55b
Gareth A Rowell <gareth_rowell@nps.gov>
2020-02-07 11:26:18 -0600

    Add files via upload

--- src/AllWellsPaths.java
+++ src/AllWellsPaths.java
@@ -51,6 +51,7 @@ public class AllWellsPaths extends Application {
 		int yhashno = 5;  
 		Line line = null;
 		Text text = null;
+		//Text avetext = null;
 		for(int i = 0; i < yhashno; ++i){
 			line = new Line(offsetX, (offsetY + 450 - (i*100)), offsetX + 20, (offsetY + 450 - (i*100)));
 			line.setStrokeWidth(2.0);
@@ -103,7 +104,7 @@ public class AllWellsPaths extends Application {
 		// get data
 		
 		int maxrow = 2636;
-		int maxcol = 9;
+		int maxcol = 10;
 		int row = 1;
 		int col = 1;
 		
@@ -111,7 +112,7 @@ public class AllWellsPaths extends Application {
 		String[][] welldata = new String[maxrow][maxcol];
 	   
 		String infile = new String();
-		infile = "C:\\Users\\Growell\\src\\\\ts\\CUVA_corr_depths_int.csv";
+		infile = "C:\\Users\\Growell\\src\\\\ts\\CUVA_well_depth_averages.csv";
 	   
 		try{
 			DataScanner datascanner = new DataScanner(infile);
@@ -122,7 +123,7 @@ public class AllWellsPaths extends Application {
 				    " " +  welldata[row][2] + " " +  welldata[row][3] + 	
 					" " +  welldata[row][4] + " " +  welldata[row][5] + 
 					" " +  welldata[row][6] + " " +  welldata[row][7] +
-					" " +  welldata[row][8]);
+					" " +  welldata[row][8] + " " +  welldata[row][9]);
 			}	  
 		}	  
 		catch (FileNotFoundException ex){
@@ -136,8 +137,13 @@ public class AllWellsPaths extends Application {
 		
 		
 		Path path = new Path();
+		Path avepath = new Path();
 		path.setStroke(Color.RED);
-		path.setStrokeWidth(2);
+		path.setStrokeWidth(1);
+		avepath.setStroke(Color.BLUE);
+		avepath.setStrokeWidth(2);	
+		
+		
 
         Integer jdate = null;
 		Integer flag = null;
@@ -153,7 +159,7 @@ public class AllWellsPaths extends Application {
 		System.out.println("Where: top " + row + " " + col);
 		
 		while( col < 8 ){
-			System.out.println("Where: Inside col while " + row + " " + col);
+			//System.out.println("Where: Inside col while " + row + " " + col);
 			 // start new path
 			
 			jdate = (Integer.parseInt(welldata[row][0]))/2;
@@ -161,18 +167,18 @@ public class AllWellsPaths extends Application {
 			flag = (Integer.parseInt(welldata[row][col]));	
 			
 			
-			System.out.println("Where: Just before path while " + row + " " + col);
+			//System.out.println("Where: Just before path while " + row + " " + col);
 			
 			while( row < maxrow){
 				
 				//start new path
-				System.out.println("Where: Inside row while - 1 " + row + " " + col);
+				//System.out.println("Where: Inside row while - 1 " + row + " " + col);
 				jdate = (Integer.parseInt(welldata[row][0]))/2;
 				depth = -1*(Integer.parseInt(welldata[row][col]))+(offsetY + 250);
 				flag = (Integer.parseInt(welldata[row][col]));
 				moveTo = new MoveTo(jdate, depth);	
 				path.getElements().add(moveTo); 
-				System.out.println("Where: Inside row while - 2 " + row + " " + col);
+				//System.out.println("Where: Inside row while - 2 " + row + " " + col);
 			
 				row++;
 			
@@ -181,7 +187,7 @@ public class AllWellsPaths extends Application {
 				flag = (Integer.parseInt(welldata[row][col]));		
 				while((flag != -9999) && (row < maxrow)){
 				
-					System.out.println("Where: Inside path while - 3 " + row + " " + col);
+					//System.out.println("Where: Inside path while - 3 " + row + " " + col);
 					//System.out.println("Where: Inside path while - 3  " + jdate+ " " + depth);
 					lineTo = new LineTo(jdate, depth);				
 					path.getElements().add(lineTo); 
@@ -196,32 +202,103 @@ public class AllWellsPaths extends Application {
 			
 			
 				while((flag == -9999)&& (row < maxrow)){ // and test for row < maxrow
-					System.out.println("Where: Inside -9999 while - 5 " + row + " " + col);
+					//System.out.println("Where: Inside -9999 while - 5 " + row + " " + col);
 					flag = (Integer.parseInt(welldata[row][col]));
 					row++;
 				
 				}
-				System.out.println("Where: Bottom of row while - 6 " + row + " " + col);
+				//System.out.println("Where: Bottom of row while - 6 " + row + " " + col);
 			
 			}
-			System.out.println("Where: Bottom of col while - 7 " + row + " " + col);
+			//System.out.println("Where: Bottom of col while - 7 " + row + " " + col);
 			col++;
 			row = 1;
 		}
 		
+		
+		// just do the average
+		
+		row = 1;
+		col = 9;
+		
+		//System.out.println("Where: Inside col while " + row + " " + col);
+		// start new path
+			
+		jdate = (Integer.parseInt(welldata[row][0]))/2;
+		depth = -1*(Integer.parseInt(welldata[row][col]))+(offsetY + 250);
+		flag = (Integer.parseInt(welldata[row][col]));	
+			
+		
+		//System.out.println("Where: Just before path while " + row + " " + col);
+			
+		while( row < maxrow){
+			
+			//start new path
+			//System.out.println("Where: Inside row while - 1 " + row + " " + col);
+			jdate = (Integer.parseInt(welldata[row][0]))/2;
+			depth = -1*(Integer.parseInt(welldata[row][col]))+(offsetY + 250);
+			flag = (Integer.parseInt(welldata[row][col]));
+			moveTo = new MoveTo(jdate, depth);	
+			path.getElements().add(moveTo); 
+			//System.out.println("Where: Inside row while - 2 " + row + " " + col);
+			
+				row++;
+			
+				jdate = (Integer.parseInt(welldata[row][0]))/2;
+				depth = -1*(Integer.parseInt(welldata[row][col]))+(offsetY + 250);
+				flag = (Integer.parseInt(welldata[row][col]));		
+			while((flag != -9999) && (row < maxrow)){
+				
+				//System.out.println("Where: Inside path while - 3 " + row + " " + col);
+				//System.out.println("Where: Inside path while - 3  " + jdate+ " " + depth);
+				lineTo = new LineTo(jdate, depth);				
+				avepath.getElements().add(lineTo); 
+				row++;
+				
+				jdate = (Integer.parseInt(welldata[row][0]))/2;
+				depth = -1*(Integer.parseInt(welldata[row][col]))+(offsetY + 250);
+				flag = (Integer.parseInt(welldata[row][col]));
+				//System.out.println("Where: Inside path while - 4 " + row + " " + col);
+			}
+				
+			
+			
+			while((flag == -9999)&& (row < maxrow)){ // and test for row < maxrow
+				//System.out.println("Where: Inside -9999 while - 5 " + row + " " + col);
+				flag = (Integer.parseInt(welldata[row][col]));
+				row++;
+				
+			}
+			
+			System.out.println("Where: Bottom of row while - " + row + " " + col);
+			
+		}
+		//
+		
 		// well label
 		col = 1;
 		text = new Text();
-		text.setText(welldata[0][col]);
+		text.setText("Individual wells");
 		text.setX(1460);
 		text.setY(480);
 		text.setStrokeWidth(1);
 		text.setStroke(Color.RED);
-		text.setFont(Font.font("helvetica", 40));
+		text.setFont(Font.font("helvetica", 30));
 		text.setFill(Color.RED);
 		group.getChildren().add(text);
 		
+		Text avetext = new Text();
+		avetext.setText("Average");
+		avetext.setX(1460);
+		avetext.setY(530);
+		avetext.setStrokeWidth(1);
+		avetext.setStroke(Color.BLUE);
+		avetext.setFont(Font.font("helvetica", 30));
+		avetext.setFill(Color.BLUE);
+		group.getChildren().add(avetext);
+		
 		group.getChildren().add(path);
+		group.getChildren().add(avepath);
 		
 		Scene scene = new Scene(group, 1850 + offsetX, 600, Color.BLACK);  
       
